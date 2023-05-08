bb05ba22c42934fdf94f1418186e053fa4bbeb27
NewBeez001 <aldrinpads92@gmail.com>
2023-05-08 13:04:54 +0800

    Add: Added controller, model and routes for products

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./db');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.send('Server is running');
